7b93478628d8be20898ea959f316d5813cb83fbf
James R. Barlow <jim@purplerock.ca>
2019-05-14 12:17:02 -0700

    Try sitecustomize

--- src/ocrmypdf/__main__.py
+++ src/ocrmypdf/__main__.py
@@ -931,12 +931,6 @@ def check_environ(options, _log):
             )
     if 'PYTEST_CURRENT_TEST' in os.environ:
         os.environ['_OCRMYPDF_TEST_INFILE'] = options.input_file
-    if 'TRAVIS' in os.environ:
-        os.environ['COVERAGE_PROCESS_START'] = '.coveragerc'
-        with suppress(ImportError):
-            import coverage
-
-            coverage.process_startup()
     if 'COV_CORE_SOURCE' in os.environ:
         with suppress(ImportError):
             from pytest_cov.embed import cleanup_on_sigterm
